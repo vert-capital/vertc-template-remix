4778007381f0170d66198d655190124daab57c10
Chiu Yong <chiu@vert-capital.com>
2024-03-27 14:37:05 -0300

    add absence new resource

---
+++ app/routes/api+/absences.new.tsx
@@ -0,0 +1,14 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { handleError } from "@vert-capital/common";
+import { AbsenceService } from "~/services/absence.service";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  try {
+    const service = new AbsenceService();
+    const response = await service.new(request);
+    return json(response);
+  } catch (error) {
+    return json({ error: handleError(error) });
+  }
+}
