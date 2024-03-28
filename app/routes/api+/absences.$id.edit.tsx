3018a6679dfa373de65c6548082a0f3b9c2ec2d6
Chiu Yong <chiu@vert-capital.com>
2024-03-28 14:32:41 -0300

    fix absence edit resource

--- app/routes/api+/absences.$id.edit.tsx
+++ app/routes/api+/absences.$id.edit.tsx
@@ -1,13 +1,26 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { ActionFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { handleError } from "@vert-capital/common";
+import { formDataValues, handleError } from "@vert-capital/common";
+import invariant from "tiny-invariant";
+import { AbsencePostModel } from "~/models/absence.model";
 import { AbsenceService } from "~/services/absence.service";
 
-export async function loader({ request }: LoaderFunctionArgs) {
+export async function action({ request, params }: ActionFunctionArgs) {
+  const absenceId = params.id;
+  invariant(absenceId, "Absence ID is required");
+  const { ...values } = await formDataValues({ request });
   try {
     const service = new AbsenceService();
-    const response = await service.update(request);
-    return json(response);
+    await service.update(absenceId, values);
+    return json({
+      nome: "",
+      email: "",
+      time: "",
+      gestor: "",
+      tipo: "",
+      dataInicio: "",
+      dataFim: "",
+    } as AbsencePostModel);
   } catch (error) {
     return json({ error: handleError(error) });
   }
